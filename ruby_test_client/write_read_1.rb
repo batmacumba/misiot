8e5de3c2066ac6390d338225779d296450854819
bruno carneiro <brunocarneirodacunha@usp.br>
2020-04-01 17:58:22 -0300

    testes prontos

---
+++ ruby_test_client/write_read_1.rb
@@ -0,0 +1,62 @@
+require 'paho-mqtt'
+
+start_time = 0
+message_counter = 0
+elapsed_times = []
+waiting_message = true
+waiting_suback = true
+waiting_puback = true
+
+client = PahoMqtt::Client.new
+
+client.on_message do |message|
+	elapsed_time = Time.now - start_time
+	message_counter += 1
+	elapsed_times << elapsed_time
+	waiting_message = false
+end
+
+### Register a callback on suback to assert the subcription
+client.on_suback do
+  waiting_suback = false
+end
+
+### Register a callback for puback event when receiving a puback
+client.on_puback do
+  waiting_puback = false
+end
+
+# Connect to the eclipse test server on port 1883 (Unencrypted mode)
+client.connect('127.0.0.1', 1883)
+
+### Subscribe to a topic
+client.subscribe(['/mqtt/teste', 2])
+
+### Waiting for the suback answer and excute the previously set on_suback callback
+while waiting_suback do
+  sleep 0.001
+end
+
+while message_counter < 1000 do
+	waiting_message = true
+	start_time = Time.now
+	# Publlish a message on the topic "/paho/ruby/test" with "retain == false" and "qos == 1"
+	client.publish("/mqtt/teste", "1", false, 1)
+	while waiting_message do
+	  sleep 0.001
+	end
+end
+
+i = 0
+while i < 1000 do
+	elapsed_in_ms = (elapsed_times[i] * 1000).round(3)
+	print "#{elapsed_in_ms}\n"
+	i += 1
+end
+
+# average_time = ((elapsed_times.inject(0){|sum,x| sum + x } / elapsed_times.size) * 1000).round(3)
+# print "sample size: #{elapsed_times.size}\n"
+# print "average ms for message arrival: #{average_time}\n"
+
+### Calling an explicit disconnect
+client.disconnect
--- ruby_test_client/write_read_string.rb
+++ ruby_test_client/write_read_string.rb
@@ -1,9 +1,11 @@
 require 'paho-mqtt'
 
 start_time = 0
-ack_counter = 0
-waiting_puback = true
+message_counter = 0
 elapsed_times = []
+waiting_message = true
+waiting_suback = true
+waiting_puback = true
 
 # String aleatória
 o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
@@ -11,24 +13,40 @@ string = (0...1000).map { o[rand(o.length)] }.join
 
 client = PahoMqtt::Client.new
 
-# Callback para a mensagem puback
+client.on_message do |message|
+	elapsed_time = Time.now - start_time
+	message_counter += 1
+	elapsed_times << elapsed_time
+	waiting_message = false
+end
+
+### Register a callback on suback to assert the subcription
+client.on_suback do
+  waiting_suback = false
+end
+
+### Register a callback for puback event when receiving a puback
 client.on_puback do
-  elapsed_time = Time.now - start_time
-  # puts "message #{ack_counter}: #{elapsed_time}"
   waiting_puback = false
-  ack_counter += 1
-  elapsed_times << elapsed_time
 end
 
 # Connect to the eclipse test server on port 1883 (Unencrypted mode)
 client.connect('127.0.0.1', 1883)
 
-while ack_counter < 1000 do
-	waiting_puback = true
+### Subscribe to a topic
+client.subscribe(['/mqtt/teste', 2])
+
+### Waiting for the suback answer and excute the previously set on_suback callback
+while waiting_suback do
+  sleep 0.001
+end
+
+while message_counter < 1000 do
+	waiting_message = true
 	start_time = Time.now
 	# Publlish a message on the topic "/paho/ruby/test" with "retain == false" and "qos == 1"
 	client.publish("/mqtt/teste", string, false, 1)
-	while waiting_puback do
+	while waiting_message do
 	  sleep 0.001
 	end
 end
@@ -42,7 +60,7 @@ end
 
 # average_time = ((elapsed_times.inject(0){|sum,x| sum + x } / elapsed_times.size) * 1000).round(3)
 # print "sample size: #{elapsed_times.size}\n"
-# print "average ms for puback: #{average_time}\n"
+# print "average ms for message arrival: #{average_time}\n"
 
 ### Calling an explicit disconnect
 client.disconnect
