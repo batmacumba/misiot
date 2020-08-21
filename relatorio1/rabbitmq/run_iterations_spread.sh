91f53490f0806ab1f28200b5e01a6957ca172f63
Bruno Carneiro <brunocarneirodacunha@usp.br>
2020-08-21 03:48:31 +0000

    resultados remote

--- relatorio1/rabbitmq/run_iterations_spread.sh
+++ relatorio1/rabbitmq/run_iterations_spread.sh
@@ -1,12 +1,12 @@
-# for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-# do
-#   sh run_spread.sh 127.0.0.1 $i
-# done
-
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-  sh run_spread.sh 10.144.0.3 $i
+  sh run_spread.sh 127.0.0.1 $i
 done
+
+# for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+# do
+#   sh run_spread.sh 10.144.0.3 $i
+# done
 
 #for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 #do
