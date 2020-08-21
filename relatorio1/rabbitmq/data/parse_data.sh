35e224d202f67d4d58cff45568fe714305ea268c
Bruno Carneiro <brunocarneirodacunha@usp.br>
2020-08-21 01:13:41 -0300

    dados processados spread

--- relatorio1/rabbitmq/data/parse_data.sh
+++ relatorio1/rabbitmq/data/parse_data.sh
@@ -1,230 +1,50 @@
 
-echo "local mosquitto 05_plain_spread_10 mean"
-python parse_data.py local mosquitto 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "local mosquitto 06_plain_spread_50 mean"
-python parse_data.py local mosquitto 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "local mosquitto 07_plain_spread_100 mean"
-python parse_data.py local mosquitto 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "local mosquitto  08_plain_spread_200 mean"
-python parse_data.py local mosquitto 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "local mosquitto 13_ssl_spread_10 mean"
-python parse_data.py local mosquitto 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "local mosquitto 14_ssl_spread_50 mean"
-python parse_data.py local mosquitto 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "local mosquitto 15_ssl_spread_100 mean"
-python parse_data.py local mosquitto 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "local mosquitto 16_ssl_spread_200 mean"
-python parse_data.py local mosquitto 16_ssl_spread_200 mean | python process_results.py
-read varname
+echo "local rabbitmq 05_plain_spread_10 mean"
+python3 parse_data.py local rabbitmq 05_plain_spread_10 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 06_plain_spread_50 mean"
+python3 parse_data.py local rabbitmq 06_plain_spread_50 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 07_plain_spread_100 mean"
+python3 parse_data.py local rabbitmq 07_plain_spread_100 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq  08_plain_spread_200 mean"
+python3 parse_data.py local rabbitmq 08_plain_spread_200 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 13_ssl_spread_10 mean"
+python3 parse_data.py local rabbitmq 13_ssl_spread_10 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 14_ssl_spread_50 mean"
+python3 parse_data.py local rabbitmq 14_ssl_spread_50 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 15_ssl_spread_100 mean"
+python3 parse_data.py local rabbitmq 15_ssl_spread_100 mean | python3 process_results.py
+#read varname
+echo "local rabbitmq 16_ssl_spread_200 mean"
+python3 parse_data.py local rabbitmq 16_ssl_spread_200 mean | python3 process_results.py
+#read varname
 
-echo "wifi mosquitto 05_plain_spread_10 mean"
-python parse_data.py wifi mosquitto 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "wifi mosquitto 06_plain_spread_50 mean"
-python parse_data.py wifi mosquitto 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "wifi mosquitto 07_plain_spread_100 mean"
-python parse_data.py wifi mosquitto 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "wifi mosquitto  08_plain_spread_200 mean"
-python parse_data.py wifi mosquitto 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "wifi mosquitto 13_ssl_spread_10 mean"
-python parse_data.py wifi mosquitto 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "wifi mosquitto 14_ssl_spread_50 mean"
-python parse_data.py wifi mosquitto 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "wifi mosquitto 15_ssl_spread_100 mean"
-python parse_data.py wifi mosquitto 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "wifi mosquitto 16_ssl_spread_200 mean"
-python parse_data.py wifi mosquitto 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-echo "ethernet mosquitto 05_plain_spread_10 mean"
-python parse_data.py ethernet mosquitto 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 06_plain_spread_50 mean"
-python parse_data.py ethernet mosquitto 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 07_plain_spread_100 mean"
-python parse_data.py ethernet mosquitto 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "ethernet mosquitto  08_plain_spread_200 mean"
-python parse_data.py ethernet mosquitto 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 13_ssl_spread_10 mean"
-python parse_data.py ethernet mosquitto 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 14_ssl_spread_50 mean"
-python parse_data.py ethernet mosquitto 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 15_ssl_spread_100 mean"
-python parse_data.py ethernet mosquitto 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "ethernet mosquitto 16_ssl_spread_200 mean"
-python parse_data.py ethernet mosquitto 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-################################################################################
-
-echo "local aedes 05_plain_spread_10 mean"
-python parse_data.py local aedes 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "local aedes 06_plain_spread_50 mean"
-python parse_data.py local aedes 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "local aedes 07_plain_spread_100 mean"
-python parse_data.py local aedes 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "local aedes  08_plain_spread_200 mean"
-python parse_data.py local aedes 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "local aedes 13_ssl_spread_10 mean"
-python parse_data.py local aedes 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "local aedes 14_ssl_spread_50 mean"
-python parse_data.py local aedes 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "local aedes 15_ssl_spread_100 mean"
-python parse_data.py local aedes 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "local aedes 16_ssl_spread_200 mean"
-python parse_data.py local aedes 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-echo "wifi aedes 05_plain_spread_10 mean"
-python parse_data.py wifi aedes 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "wifi aedes 06_plain_spread_50 mean"
-python parse_data.py wifi aedes 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "wifi aedes 07_plain_spread_100 mean"
-python parse_data.py wifi aedes 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "wifi aedes  08_plain_spread_200 mean"
-python parse_data.py wifi aedes 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "wifi aedes 13_ssl_spread_10 mean"
-python parse_data.py wifi aedes 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "wifi aedes 14_ssl_spread_50 mean"
-python parse_data.py wifi aedes 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "wifi aedes 15_ssl_spread_100 mean"
-python parse_data.py wifi aedes 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "wifi aedes 16_ssl_spread_200 mean"
-python parse_data.py wifi aedes 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-echo "ethernet aedes 05_plain_spread_10 mean"
-python parse_data.py ethernet aedes 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "ethernet aedes 06_plain_spread_50 mean"
-python parse_data.py ethernet aedes 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "ethernet aedes 07_plain_spread_100 mean"
-python parse_data.py ethernet aedes 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "ethernet aedes  08_plain_spread_200 mean"
-python parse_data.py ethernet aedes 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "ethernet aedes 13_ssl_spread_10 mean"
-python parse_data.py ethernet aedes 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "ethernet aedes 14_ssl_spread_50 mean"
-python parse_data.py ethernet aedes 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "ethernet aedes 15_ssl_spread_100 mean"
-python parse_data.py ethernet aedes 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "ethernet aedes 16_ssl_spread_200 mean"
-python parse_data.py ethernet aedes 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-
-################################################################################
-
-echo "local hivemq 05_plain_spread_10 mean"
-python parse_data.py local hivemq 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "local hivemq 06_plain_spread_50 mean"
-python parse_data.py local hivemq 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "local hivemq 07_plain_spread_100 mean"
-python parse_data.py local hivemq 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "local hivemq  08_plain_spread_200 mean"
-python parse_data.py local hivemq 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "local hivemq 13_ssl_spread_10 mean"
-python parse_data.py local hivemq 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "local hivemq 14_ssl_spread_50 mean"
-python parse_data.py local hivemq 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "local hivemq 15_ssl_spread_100 mean"
-python parse_data.py local hivemq 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "local hivemq 16_ssl_spread_200 mean"
-python parse_data.py local hivemq 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-echo "wifi hivemq 05_plain_spread_10 mean"
-python parse_data.py wifi hivemq 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "wifi hivemq 06_plain_spread_50 mean"
-python parse_data.py wifi hivemq 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "wifi hivemq 07_plain_spread_100 mean"
-python parse_data.py wifi hivemq 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "wifi hivemq  08_plain_spread_200 mean"
-python parse_data.py wifi hivemq 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "wifi hivemq 13_ssl_spread_10 mean"
-python parse_data.py wifi hivemq 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "wifi hivemq 14_ssl_spread_50 mean"
-python parse_data.py wifi hivemq 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "wifi hivemq 15_ssl_spread_100 mean"
-python parse_data.py wifi hivemq 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "wifi hivemq 16_ssl_spread_200 mean"
-python parse_data.py wifi hivemq 16_ssl_spread_200 mean | python process_results.py
-read varname
-
-echo "ethernet hivemq 05_plain_spread_10 mean"
-python parse_data.py ethernet hivemq 05_plain_spread_10 mean | python process_results.py
-read varname
-echo "ethernet hivemq 06_plain_spread_50 mean"
-python parse_data.py ethernet hivemq 06_plain_spread_50 mean | python process_results.py
-read varname
-echo "ethernet hivemq 07_plain_spread_100 mean"
-python parse_data.py ethernet hivemq 07_plain_spread_100 mean | python process_results.py
-read varname
-echo "ethernet hivemq  08_plain_spread_200 mean"
-python parse_data.py ethernet hivemq 08_plain_spread_200 mean | python process_results.py
-read varname
-echo "ethernet hivemq 13_ssl_spread_10 mean"
-python parse_data.py ethernet hivemq 13_ssl_spread_10 mean | python process_results.py
-read varname
-echo "ethernet hivemq 14_ssl_spread_50 mean"
-python parse_data.py ethernet hivemq 14_ssl_spread_50 mean | python process_results.py
-read varname
-echo "ethernet hivemq 15_ssl_spread_100 mean"
-python parse_data.py ethernet hivemq 15_ssl_spread_100 mean | python process_results.py
-read varname
-echo "ethernet hivemq 16_ssl_spread_200 mean"
-python parse_data.py ethernet hivemq 16_ssl_spread_200 mean | python process_results.py
-read varname
+echo "remote rabbitmq 05_plain_spread_10 mean"
+python3 parse_data.py remote rabbitmq 05_plain_spread_10 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 06_plain_spread_50 mean"
+python3 parse_data.py remote rabbitmq 06_plain_spread_50 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 07_plain_spread_100 mean"
+python3 parse_data.py remote rabbitmq 07_plain_spread_100 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq  08_plain_spread_200 mean"
+python3 parse_data.py remote rabbitmq 08_plain_spread_200 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 13_ssl_spread_10 mean"
+python3 parse_data.py remote rabbitmq 13_ssl_spread_10 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 14_ssl_spread_50 mean"
+python3 parse_data.py remote rabbitmq 14_ssl_spread_50 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 15_ssl_spread_100 mean"
+python3 parse_data.py remote rabbitmq 15_ssl_spread_100 mean | python3 process_results.py
+#read varname
+echo "remote rabbitmq 16_ssl_spread_200 mean"
+python3 parse_data.py remote rabbitmq 16_ssl_spread_200 mean | python3 process_results.py
+#read varname
