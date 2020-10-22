5d75d1287b200b88fdd94dd7209fda71f44ac902
batmacumba <brunocarneirodacunha@usp.br>
2020-10-22 17:41:38 -0300

    merge

--- relatorio2/webhook/server.js
+++ relatorio2/webhook/server.js
@@ -2,9 +2,14 @@ const express = require( 'express' );
 const app = express();
 app.use( express.json() );
 
-app.post( '/', ( req, res ) => {
-    console.log( 'received webhook', req.body );
+app.get( '/', ( req, res ) => {
+    console.log( 'received GET', req.body );
     res.sendStatus( 200 );
 } );
 
-app.listen( 80, () => console.log( 'Node.js server started on port 80.' ) );
+app.post( '/', ( req, res ) => {
+    console.log( 'received POST', req.body );
+    res.sendStatus( 200 );
+} );
+
+app.listen( 8877, () => console.log( 'Node.js server started on port 8877.' ) );
