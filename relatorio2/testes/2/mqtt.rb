8730c163ca69fc0d7997bf47efbbc24e8c0bbe8f
batmacumba <brunocarneirodacunha@usp.br>
2020-11-06 04:23:19 -0300

    quase

--- relatorio2/testes/2/mqtt.rb
+++ relatorio2/testes/2/mqtt.rb
@@ -1,17 +1,65 @@
-require 'rubygems'
-require 'mqtt'
-require 'json'
+require 'httparty'
+require 'paho-mqtt'
 
-payload = 
-{
-    "data": [
-        {
-            "temperature": 99,
-            "timestamp": "2017-06-14T17:52:25.428Z"
-        }
-    ]
-}.to_json
+N = 1000
+# Criação de um novo resource
+response = HTTParty.post('http://127.0.0.1:3003/resources/', 
+	:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
+	:body => {
+			  "data": {
+			    "description": "Atuador",
+			    "capabilities": [
+			      "illuminate",
+			    ],
+			    "status": "active",
+			    "lat": -23.559616,
+			    "lon": -46.731386
+			  }
+			}.to_json)
 
-MQTT::Client.connect('127.0.0.1') do |c|
-  c.publish('resources/0c350791-4971-46a0-8b00-9034df38a36c/temperature', payload)
+# Handling da resposta
+if response.code != 201
+	puts "Não foi possível criar um novo resource"
+	return
 end
+uuid = JSON.parse(response.body)['data']['uuid']
+
+# Abre o cliente MQTT
+client = PahoMqtt::Client.new()
+client.connect('127.0.0.1', 1883)
+client.subscribe(['commands/' + uuid, 2])
+while @waiting_suback do
+	sleep 0.001
+end
+
+start_time = 0
+elapsed_time = 0
+waiting_for_command = false
+
+client.on_message do |message|
+	elapsed_time = ((Time.now - start_time) * 1000).round(3)
+	waiting_for_command = false
+end
+
+# Envio das N mensagens
+N.times {
+	HTTParty.post('http://127.0.0.1:3000/commands', 
+		:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
+		:body => {
+				  "data": [
+				    {
+				      "uuid": "#{uuid}",
+				      "capabilities": {
+				        "illuminate": "on"
+				      }
+				    }
+				  ]
+				}.to_json)
+	start_time = Time.now()
+	waiting_for_command = true
+	while waiting_for_command
+		nil
+	end
+	print("#{elapsed_time}\n")
+}
+
