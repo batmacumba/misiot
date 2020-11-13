66dcab464d3959f6853b5277c2fc87eb822e2d55
batmacumba <brunocarneirodacunha@usp.br>
2020-11-13 18:43:02 -0300

    teste 2

--- relatorio2/testes/2/http.rb
+++ relatorio2/testes/2/http.rb
@@ -2,6 +2,24 @@ require 'httparty'
 require 'socket'
 
 N = 1000
+
+# # Criação da capability "time"
+# response = HTTParty.post('http://127.0.0.1:3003/capabilities/', 
+# 	:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
+# 	:body => {
+# 				"name": "start_time",
+# 				"description": "Holds a timestamp for the test",
+# 				"capability_type": "sensor"
+# 			 }.to_json)
+
+# # Handling da resposta
+# if response.code != 201
+# 	puts "Não foi possível criar um novo resource"
+# 	return
+# end
+# uuid = JSON.parse(response.body)['data']['uuid']
+
+
 # Criação de um novo resource
 response = HTTParty.post('http://127.0.0.1:3003/resources/', 
 	:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
@@ -44,15 +62,17 @@ if response.code != 201
 	return
 end
 
-# Criação do Webhook
-server = TCPServer.new 5678
-waiting_for_command = false
-elapsed_time = 0
-start_time = 0
 
-t = Thread.new {
+# Variáveis globais
+elapsed_time = Array.new(1000, 0)
+start_time = Array.new(1000, 0)
+i = 0
+
+# Thread com o webhook que receberá os comandos da plataforma
+t1 = Thread.new {
+	server = TCPServer.new 5678
 	while session = server.accept
-	  elapsed_time = ((Time.now - start_time) * 1000).round(3)
+	  	elapsed_time[i] = ((Time.now - start_time[i]) * 1000).round(3)
 	  	body = "Ok"
 		head = "HTTP/1.1 200\r\n" \
 		"Date: #{Time.now.httpdate}\r\n" \
@@ -62,30 +82,33 @@ t = Thread.new {
 		session.write "\r\n"
 		session.write body
 		session.close
-	  waiting_for_command = false
 	end
 }
 
-# Envio das N mensagens
-N.times {
-	HTTParty.post('http://127.0.0.1:3000/commands', 
-		:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
-		:body => {
-				  "data": [
-				    {
-				      "uuid": "#{uuid}",
-				      "capabilities": {
-				        "illuminate": "on"
-				      }
-				    }
-				  ]
-				}.to_json)
-	start_time = Time.now()
-	waiting_for_command = true
-	while waiting_for_command
-		nil
-	end
-	print("#{elapsed_time}\n")
+# Thread que enviará os comandos à plataforma
+t2 = Thread.new {
+	N.times {
+		start_time[i] = Time.now()
+		HTTParty.post('http://127.0.0.1:3000/commands', 
+			:headers => {'cache-control': 'no-cache','content-type': 'application/json'}, 
+			:body => {
+					  "data": [
+					    {
+					      "uuid": "#{uuid}",
+					      "capabilities": {
+					        "illuminate": "on"
+					      }
+					    }
+					  ]
+					}.to_json)
+		while elapsed_time[i] == 0
+			nil
+		end
+		print("#{elapsed_time[i]}\n")
+		i += 1
+	}
 }
 
-t.join
+
+t1.join
+t2.join
